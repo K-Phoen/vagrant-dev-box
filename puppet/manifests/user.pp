4a46ec3418ec7ba6ac898e61b129e96bb508a5ef
kphoen <geek63@gmail.com>
2013-01-16 12:27:04 +0100

    Add: clone my config files

--- puppet/manifests/user.pp
+++ puppet/manifests/user.pp
@@ -1,6 +1,14 @@
 class user {
   $devPackages = [ 'vim', 'zsh', 'curl', 'git', 'most', 'tree', 'htop', ]
   package { $devPackages:
-    ensure  => 'installed',
+    ensure  => 'latest',
+  }
+
+  exec { '/usr/bin/git clone --recursive git://github.com/K-Phoen/Config.git':
+    cwd     => '/home/vagrant',
+    creates => '/home/vagrant/Config',
+    unless  => '/usr/bin/test -d /home/vagrant/Config',
+    user    => 'vagrant',
+    group   => 'vagrant',
   }
 }
